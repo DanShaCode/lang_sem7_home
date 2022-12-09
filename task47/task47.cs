587a976f10271bff63b611679568a88c42b3f366
Danis Sharipov <danshadance@mail.ru>
2022-12-09 16:53:41 +0600

    Начал делать 47 задачу

--- task47/task47.cs
+++ task47/task47.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void RandomFillArray (double [,] newArray)
+{
+    Console.Write("Полученный двумерный массив:");
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i, j] = new Random().Next(1,10);
+            Console.Write($" {newArray[i, j]} ");
+        }
+    }
+}
+
+int userDataM = Convert.ToInt32(Console.ReadLine());
+int userDataN = Convert.ToInt32(Console.ReadLine());
+
+double [,] myArray =  new double [userDataM, userDataN];
+
+RandomFillArray(myArray);
