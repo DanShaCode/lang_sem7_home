f766bd7a6e96bc4ac9340477b70ad61109f9b69b
Danis Sharipov <danshadance@mail.ru>
2022-12-09 19:04:42 +0600

    Практически завершил задачу 47

--- task47/task47.cs
+++ task47/task47.cs
@@ -4,22 +4,29 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-void RandomFillArray (double [,] newArray)
+void CreateRandomArray (double [,] newArray)
 {
-    Console.Write("Полученный двумерный массив:");
+    Console.WriteLine("Полученный двумерный массив:");
+    Console.WriteLine();
     for (int i = 0; i < newArray.GetLength(0); i++)
     {
         for (int j = 0; j < newArray.GetLength(1); j++)
         {
-            newArray[i, j] = new Random().Next(1,10);
-            Console.Write($" {newArray[i, j]} ");
+            newArray[i, j] = new Random().NextDouble() * 10;
+            Console.Write($" {Math.Round(newArray[i, j], 1)} ");
         }
+        Console.WriteLine();
     }
 }
-
+Console.WriteLine();
+Console.WriteLine("Задайте двумерный массив размером M*N, заполненный случайными вещественными числами.");
+Console.WriteLine();
+Console.Write("Введите число M: ");
 int userDataM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
 int userDataN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
 double [,] myArray =  new double [userDataM, userDataN];
 
-RandomFillArray(myArray);
+CreateRandomArray(myArray);
