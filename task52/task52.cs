f1fffc3fac0fa32460dd4a7dad08245775547762
Danis Sharipov <danshadance@mail.ru>
2022-12-10 22:07:51 +0600

    Фикс 52 задачи

--- task52/task52.cs
+++ task52/task52.cs
@@ -5,7 +5,7 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
 
- void RandomFillArray (int [,] num) // Заполняем массив
+ void RandomFillArray (int [,] num) // Наполняем массив случайными числами
  {
     for (int i = 0; i < num.GetLength(0); i++)
     {
@@ -18,7 +18,7 @@
     }
  }
 
-  void ColMidSum (int [,] colSum) // Суммируем столбцы массива
+  void ColMidSum (int [,] colSum) // Суммируем столбцы массива, находим СА и выводим в консоль
 {
     int cols = colSum.GetLength(0);
     for (int j = 0; j < colSum.GetLength(1); j++)
@@ -28,21 +28,26 @@
         {
             sum = sum + colSum[i, j];
         }
-        Console.Write($"{sum / cols} ");
+        Console.Write($"{Math.Pow(sum / cols, 1)}; ");
     }
 }
 
 Console.WriteLine();
 Console.WriteLine("Задайте параметры массива.");
 Console.WriteLine();
+
 Console.Write("Введите количество строк: ");
 var userRow = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 var userCol = Convert.ToInt32(Console.ReadLine());
 int [,] numbers = new int [userRow, userCol];
+
 Console.WriteLine();
 Console.WriteLine("Массив успешно создан!"); 
 Console.WriteLine();
 
 RandomFillArray(numbers);
+Console.WriteLine();
+Console.Write("Среднее арифметическое каждого столбца: ");
 ColMidSum(numbers);
+Console.WriteLine();
